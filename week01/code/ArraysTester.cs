8f1465afbdb05407adc6363cdb3bad2c3814600d
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2024-05-27 12:12:48 +0200

    finished

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -39,7 +39,16 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return new double[0]; // replace this return statement with your own
+              var listOfMultiples = new double[length];
+        double numbers = 0;
+        
+        for (int i = 0; i <= length - 1; i++) 
+        {
+            numbers += number;
+            listOfMultiples[i] = numbers;
+        }
+
+        return listOfMultiples; 
     }
     
     /// <summary>
@@ -56,6 +65,13 @@ public static class ArraysTester {
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+List<int> newList = new List<int>();
 
+        newList = data.GetRange(0, data.Count - amount);
+        data.RemoveRange(0, data.Count - amount);
+
+        for (int i = 0; i < newList.Count; i++) {
+            data.Add(newList[i]);
+        }
     }
 }
